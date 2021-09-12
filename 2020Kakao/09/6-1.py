688d5080a4af8a201f10e102e333b721d91a06fd
공민상 <jka156@naver.com>
2021-09-12 22:58:05 +0900

    09

--- 2020Kakao/09/6-1.py
+++ 2020Kakao/09/6-1.py
@@ -1,6 +1,7 @@
 def solution(board, skill):
     answer = 0
-
+    n = len(board)
+    m = len(board[0])
     for k in range(len(skill)):
         sort, r1, c1, r2, c2, degree = skill[k]
         if sort == 1:
@@ -16,4 +17,10 @@ def solution(board, skill):
         for j in range(len(board[i])):
             if board[i][j] > 0 :
                 answer += 1
+    for i in range(n):
+        print(*board[i])
     return answer
+
+board = [[5,5,5,5,5],[5,5,5,5,5],[5,5,5,5,5],[5,5,5,5,5]]
+skill = [[1,0,0,3,4,4],[1,2,0,2,3,2],[2,1,0,3,1,2],[1,0,1,3,3,1]]
+print(solution(board, skill))
--- 2020Kakao/09/6-2.py
+++ 2020Kakao/09/6-2.py
@@ -1,30 +1,40 @@
 def solution(board, skill):
     answer = 0
-    prepixSum = dict()
+    n = len(board)
+    m = len(board[0])
+    prepixSum = [[0]*(m+1) for _ in range(n+1)]
 
     for k in range(len(skill)):
         sort, r1, c1, r2, c2, degree = skill[k]
-        keyword = str(r1)+' '+str(c1)+' '+str(r2)+' '+str(c2)
         if sort == 2:
-            if keyword in prepixSum:
-                prepixSum[keyword] += degree
-            else:
-                prepixSum[keyword] = degree
+            prepixSum[r1][c1] += degree
+            prepixSum[r2+1][c2+1] += degree
+            prepixSum[r1][c2+1] -= degree
+            prepixSum[r2+1][c1] -= degree
         else:
-            if keyword in prepixSum:
-                prepixSum[keyword] -= degree
-            else:
-                prepixSum[keyword] = -degree
+            prepixSum[r1][c1] -= degree
+            prepixSum[r2+1][c2+1] -= degree
+            prepixSum[r1][c2+1] += degree
+            prepixSum[r2+1][c1] += degree
 
-    for keyword in prepixSum:
-        r1, c1, r2, c2 = keyword.split(' ')
-        for i in range(int(r1), int(r2)+1):
-            for j in range(int(c1), int(c2)+1):
-                board[i][j] += prepixSum[keyword]
-    for i in range(len(board)):
-        for j in range(len(board[i])):
+    for i in range(n):
+        for j in range(m):
+            if i > 0 and j > 0:
+                prepixSum[i][j] += prepixSum[i-1][j]+prepixSum[i][j-1]-prepixSum[i-1][j-1]
+            elif i > 0:
+                prepixSum[i][j] += prepixSum[i-1][j]
+            elif j > 0 :
+                prepixSum[i][j] += prepixSum[i][j-1]
+
+    
+    for i in range(n):
+        for j in range(m):
+            board[i][j] += prepixSum[i][j] 
             if board[i][j] > 0 :
                 answer += 1
+
+    for i in range(n):
+        print(*board[i])
     return answer
 
 board = [[5,5,5,5,5],[5,5,5,5,5],[5,5,5,5,5],[5,5,5,5,5]]
