5131d0e52f556aa9da76410a035119d8c24709f5
공민상 <jka156@naver.com>
2021-09-25 21:13:30 +0900

    prefixSum

--- PrefixSum/10.py
+++ PrefixSum/10.py
@@ -1,1 +1,21 @@
-print("test")
+#https://www.acmicpc.net/problem/11659
+#백준 11659번 구간 합 구하기 4 (누적합)
+
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+nums = [0]+list(map(int, input().split()))
+dp = [0]*(n+1)
+
+dp[1] = nums[1]
+
+for i in range(2,n+1):
+    dp[i] = dp[i-1]+nums[i]
+    
+for _ in range(m):
+    a,b = map(int, input().split())
+    if a > b:
+        print(dp[a]-dp[b-1])
+    else :
+        print(dp[b]-dp[a-1])
